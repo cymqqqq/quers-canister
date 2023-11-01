437b204c827ce201e688b9bf33d0c1ea0573ff3d
jack <cymqqqq@gmail.com>
2023-11-01 19:09:06 +0800

    feat: new profile update && query method

--- src/ic_portal_canisters/src/model/profile.rs
+++ src/ic_portal_canisters/src/model/profile.rs
@@ -49,7 +49,16 @@ impl Default for QuesAns {
 }
 
 impl QuesAns {
+    // update method
+    pub fn add_profile_question(&mut self, question: &str) {
+        self.questions.push(question.into())
+    }
 
+    pub fn add_profile_answer(&mut self, answer: &str) {
+        self.answers.push(answer.into())
+    }
+
+    // query method
     pub fn get_all_questions_list(self) -> Vec<String> {
         self.questions
     }
@@ -76,16 +85,54 @@ impl Default for Profile {
 
 
 impl Profile {
-    pub fn update_description(&mut self, desc: &String) {
+    // update method
+    pub fn update_profile_description(&mut self, desc: &String) {
         self.description = desc.into();
     }
 
-
-    pub fn update_profile_info(&mut self, nickname: &String, desc: &String) {
-        self.nick_name = nickname.into();
-        self.description = desc.into();
+    pub fn update_profile_tvl(&mut self, tvl: &u32) {
+        self.tvl += tvl;
     }
 
+    pub fn update_profile_holders(&mut self, holders: &u32) {
+        self.holders += holders;
+    }
+
+    pub fn update_profile_followers(&mut self, followers: &u32) {
+        self.followers += followers;
+    }
+
+    pub fn update_profile_holding(&mut self, holding: &u32) {
+        self.holding += holding;
+    }
+
+    pub fn add_profile_question(&mut self, question: &str) {
+        self.qa_mod.add_profile_question(question);
+    }
+
+    pub fn add_profile_answer(&mut self, answer: &str) {
+        self.qa_mod.add_profile_answer(answer);
+    }
+    // query method
+    pub fn get_profile_description(&self) -> String {
+        self.description.into()
+    }
+
+    pub fn get_profile_tvl(&self) -> u32 {
+        self.tvl.into()
+    }
+
+    pub fn get_profile_holders(&self) -> u32 {
+        self.holders.into()
+    }
+
+    pub fn get_profile_holding(&self) -> u32 {
+        self.holding.into()
+    }
+
+    pub fn get_profile_followers(&self) -> u32 {
+        self.followers.into()
+    }
 }
 
 pub fn get_all() -> Vec<Profile> {
@@ -116,15 +163,6 @@ pub fn set_description(desc: String) -> Result<()> {
     Ok(())
 }
 
-pub fn set_url(urlink: String) -> Result<()> {
-    let pid = ic_cdk::api::caller();
-    PROFILE.with(|profile|
-        {
-            profile.borrow_mut().get_mut(&pid).and_then(|pro| Some(pro.update_url(urlink)))
-        }
-    );
-    Ok(())
-}
     
 pub fn update(nickname: String, desc: String) -> Result<()> {
     let pid = ic_cdk::api::caller();
