5d0f3abe1b3dfa11b03bdf4150d8a54548847e31
jack <cymqqqq@gmail.com>
2023-11-02 10:23:13 +0800

    feat: new utils module && canister env

---
+++ src/utils/src/env.rs
@@ -0,0 +1,19 @@
+use candid::Principal;
+use rand::rngs::StdRng;
+
+pub type TimestampMillis = u64;
+pub type TimestampNanos = u64;
+
+pub type CanisterId = Principal;
+pub type Cycles = u128;
+pub trait Environment {
+    fn now_nanos(&self) -> TimestampNanos;
+    fn caller(&self) -> Principal;
+    fn canister_id(&self) -> CanisterId;
+    fn cycles_balance(&self) -> Cycles;
+    fn rng(&mut self) -> &mut StdRng;
+
+    fn now(&self) -> TimestampMillis {
+        self.now_nanos() / 1_000_000
+    }
+}
