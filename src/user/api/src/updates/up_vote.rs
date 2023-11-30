68a097506ec3ba53239b6ad011788f1b75bdfb28
jack <cymqqqq@gmail.com>
2023-11-30 08:25:41 +0800

    feat: new up vote feature

---
+++ src/user/api/src/updates/up_vote.rs
@@ -0,0 +1,13 @@
+use candid::{CandidType, Principal};
+use serde::{Deserialize, Serialize};
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub struct Args {
+    pub question_id: String,
+}
+
+#[derive(CandidType, Serialize, Deserialize, Debug)]
+pub enum Response {
+    Success,
+    UpVoteInvalid,
+}
---
+++ src/user/impl/src/updates/up_vote.rs
@@ -0,0 +1,14 @@
+use crate::{mutate_state, replace_state, RuntimeState};
+use crate::guards::caller_is_quers_user;
+use ic_cdk_macros::update;
+use user_canister::up_vote::{Args, Response::*,*};
+
+#[update]
+fn up_vote(args: Args) -> Response {
+    mutate_state(|state| up_vote_impl(&args, state))
+}
+
+fn up_vote_impl(args: &Args, state: &mut RuntimeState) -> Response {
+    state.data.up_vote(&args.question_id);
+    Response::Success
+}
