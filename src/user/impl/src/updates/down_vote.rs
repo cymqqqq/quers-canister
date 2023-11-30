0c34cf439be207520f6d267835b13701cab2780f
jack <cymqqqq@gmail.com>
2023-11-30 08:25:11 +0800

    feat: new down vote feature

---
+++ src/user/impl/src/updates/down_vote.rs
@@ -0,0 +1,14 @@
+use crate::{mutate_state, replace_state, RuntimeState};
+use crate::guards::caller_is_quers_user;
+use ic_cdk_macros::update;
+use user_canister::down_vote::{Args, Response::*,*};
+
+#[update]
+fn down_vote(args: Args) -> Response {
+    mutate_state(|state| down_vote_impl(&args, state))
+}
+
+fn down_vote_impl(args: &Args, state: &mut RuntimeState) -> Response {
+    state.data.down_vote(&args.question_id);
+    Response::Success
+}
